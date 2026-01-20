ac6c2dfa5b6dbbaa9a4d75f6c630be09948dccdd
pepptyxa <pheronerxe@gmail.com>
2026-01-20 19:49:57 +0500

    Шифрование пароля. Черкашнева

--- ajax/regin_user.php
+++ ajax/regin_user.php
@@ -12,6 +12,8 @@
 	if($CheckPassword == false)
 		exit;
 
+	$password = password_hash($password, PASSWORD_DEFAULT);
+
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."'");
 	$id = -1;
 	
